f66d632a0789625266daa7ec02637166e69ddfa8
Lindanny9721 <32419597+Lindanny9721@users.noreply.github.com>
2023-03-06 14:41:11 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Chinese_Cards from './components/Chinese_Cards';
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className='Title'>Local Food Around Chinatown</h1>
+      <Chinese_Cards />  
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Chinese_Cards.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Resturant from "./Resturant";
+import Image from "./Image";
+import Description from "./Description";
+const Chinese_Cards = () => {
+return(
+    <div className="container">
+
+    </div>
+    )
+}
+export default Chinese_Cards
